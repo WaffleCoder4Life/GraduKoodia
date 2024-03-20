4d7105c69b2edf29e576add3cdf47c70752fdd37
sampsasd <sampsa.ahopelto@gmail.com>
2024-03-20 12:32:04 +0200

    NEEDS TESTING

---
+++ morningCoffee.py
@@ -0,0 +1,19 @@
+import os
+from datetime import date
+
+#MAKES NEW FOLDER FOR TODAYS SHENANIGANS
+
+parent = "Documents/Tom_Sampsa/GraduKoodia/dataAnalz/dataCollection/"
+today = date.today()
+
+day = "{:02d}".format(today.day)
+month = "{:02d}".format(today.month)
+
+newFolder = day + month + str(today.year)
+
+path = os.path.join(parent, newFolder)
+os.mkdir(path)
+
+ph = "/Photos"
+photoPath = os.path.join(path, ph)
+os.mkdir(photoPath)
